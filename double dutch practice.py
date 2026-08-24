0f501f714120f7a8c261ca5f70000c456b601501
tototomm2290 <matutomo2290@gmail.com>
2026-08-24 18:34:19 +0900

    Update double dutch practice.py

--- double dutch practice.py
+++ double dutch practice.py
@@ -9,12 +9,12 @@ from supabase import create_client
 # =========================================================
 
 st.set_page_config(
-    page_title="○×試行管理表",
+    page_title="○×練習",
     page_icon="📊",
     layout="wide"
 )
 
-st.title("📊 ○×試行管理表")
+st.title("📊 ○×練習")
 
 
 # =========================================================
